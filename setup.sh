c8d0726ed960ff2284af9a623edc7883cfce0c1c
cogitantium <falke.cs@gmail.com>
2019-09-14 00:17:29 +0200

    Add i3-centric dots and TODO-note in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,18 @@
 
 
 symlink() {
+    # TODO; implement diff-check, on diff; update repo-dot with local (or the other direction), and continue symlinking
     printf "Symlinking '%s', to path '%s'\n" "repo/$1" "$HOME/$2"
     ln -s "$(realpath ./"$1")" "$HOME/$2"
 }
 
 symlink "bashrc" ".bashrc"
+
+# Ensure that '~/.config/i3' exists before symlinking i3-centric dots
+mkdir -p "$HOME/.config/i3"
+symlink "i3/config" ".config/i3/config"
+symlink "i3/i3blocks.conf" ".config/i3/i3blocks.conf"
+symlink "i3/i3exit" ".config/i3/i3exit"
 
 # Ensure that parent dir exists before symlinking scripts
 mkdir -p "$HOME/scripts"
