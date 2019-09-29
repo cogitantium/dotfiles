a3f0ed95996757b9f1fa038dfb3747a6011245cc
cogitantium <falke.cs@gmail.com>
2019-09-29 19:22:10 +0200

    Add i3-layouts

--- setup.sh
+++ setup.sh
@@ -11,11 +11,16 @@ symlink "bashrc" ".bashrc"
 
 symlink "betterlockscreen/betterlockscreenrc" ".config/betterlockscreenrc"
 
-# Ensure that '~/.config/i3' exists before symlinking i3-centric dots
-mkdir -p "$HOME/.config/i3"
+# Ensure that '~/.config/i3/layouts' exists before symlinking i3-centric dots
+mkdir -p "$HOME/.config/i3/layouts"
 symlink "i3/config" ".config/i3/config"
 symlink "i3/i3blocks.conf" ".config/i3/i3blocks.conf"
 symlink "i3/i3exit" ".config/i3/i3exit"
+
+# Symlink i3-layouts
+symlink "i3/layouts/workspace2.json" ".config/i3/layouts/workspace2.json"
+symlink "i3/layouts/workspace4.json" ".config/i3/layouts/workspace4.json"
+symlink "i3/layouts/workspace5.json" ".config/i3/layouts/workspace5.json"
 
 # Ensure that '~/.config/fish/functions' exists before symlinking fish-centric dots
 mkdir -p "$HOME/.config/fish/functions"
